cf43bc303b5c0c9451ce47d89de959b1f382f436
Olzhas <olzhas.koshkarbay@gmail.com>
2024-02-24 17:26:17 +0600

    lab1

--- DodoPizza/app/src/main/java/com/dodopizza/MainActivity.kt
+++ DodoPizza/app/src/main/java/com/dodopizza/MainActivity.kt
@@ -1,55 +1,71 @@
 package com.dodopizza
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
 import com.dodopizza.adapter.PizzaAdapter
 import com.dodopizza.databinding.ActivityMainBinding
 import com.dodopizza.model.Pizza
 import com.dodopizza.model.PizzaDataSource
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-    private var adapter: PizzaAdapter? = null
+    private var pizza_adapter: PizzaAdapter? = null
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        PizzaAdapter(
-            onPizzaClick = {
-//                Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
-                handleMovieClick(it)
-            },
-            onPizzaRemoved ={
-                handlePizzaRemoval(it)
-            }
-        )
-        adapter  = PizzaAdapter(
-            onPizzaClick = {
-//                Toast.makeText(this, it.title, Toast.LENGTH_SHORT).show()
-                handleMovieClick(it)
-            },
-            onPizzaRemoved ={
-                handlePizzaRemoval(it)
-            }
-        )
+        val tocombo: Button = binding.toCombo
+        tocombo.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
 
-        binding.recyclerView.adapter = adapter
-        adapter?.setData(PizzaDataSource.pizzaList)
+        pizza_adapter = PizzaAdapter(
+            onPizzaClick = {
+                handlePizzaClick(it)
+            }
+        )
+        binding.pizzaView.adapter = pizza_adapter
+        pizza_adapter?.setData(PizzaDataSource.pizzaList)
+
+        binding.edtView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val searchQuery = s.toString()
+                val list = PizzaDataSource.pizzaList.filter {
+                    it.title.contains(searchQuery)
+                }
+                pizza_adapter?.setData(ArrayList(list))
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
 
     }
-    private fun handleMovieClick(pizza: Pizza){
+    private fun handlePizzaClick(pizza: Pizza){
         val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra(SecondActivity.KEY_RESULT, pizza.description)
+        intent.putExtra(SecondActivity.KEY_RESULT, pizza.title)
+        intent.putExtra(SecondActivity.KEY_INGREDIENT, pizza.description)
         intent.putExtra(SecondActivity.KEY_IMAGE, pizza.imageRes)
+        intent.putExtra(SecondActivity.KEY_PRICE, pizza.price)
         startActivity(intent)
     }
-    private fun handlePizzaRemoval(pizza: Pizza){
-        val pizzaList = PizzaDataSource.pizzaList
-        pizzaList.remove(pizza)
-        adapter?.setData(pizzaList)
-    }
+
 }
--- DodoPizza/app/src/main/java/com/dodopizza/SecondActivity.kt
+++ DodoPizza/app/src/main/java/com/dodopizza/SecondActivity.kt
@@ -1,8 +1,15 @@
 package com.dodopizza
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.dodopizza.databinding.ActivitySecondBinding
+import org.w3c.dom.Text
 import java.lang.System.load
 
 
@@ -10,7 +17,9 @@ class SecondActivity : AppCompatActivity() {
 
     companion object {
         const val KEY_RESULT = "KeyResult"
+        const val KEY_INGREDIENT = "KeyIngredient"
         const val KEY_IMAGE = "KeyImageUrl"
+        const val KEY_PRICE = "KeyPrice"
     }
 
 
@@ -21,14 +30,73 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val result = intent.getStringExtra(KEY_RESULT)
-//        val resultImage = intent.getStringExtra(KEY_IMAGE)
+        val resultIng = intent.getStringExtra(KEY_INGREDIENT)
+        val resultImage = intent.getIntExtra(KEY_IMAGE, 0)
+        val resultPrice = intent.getStringExtra(KEY_PRICE)
 
         binding.resultTextView.text = result
-//        Glide.with(this)
-//            .load(resultImage) // Load the image from the URL
-//            .into(binding.imageView)
+        binding.resultIngredient.text = resultIng
+        Glide.with(this)
+            .load(resultImage) // Load the image from the URL
+            .into(binding.imageView)
 
+        val small: Button = binding.small
+        val ave: Button = binding.avg
+        val big: Button = binding.big
 
+        val traditional: Button = binding.traditional
+        val thin: Button = binding.thin
+
+        var resultSize: TextView = binding.resultSize
+        var resultDough: TextView = binding.resultDough
+
+        val color_gray = Color.parseColor("#F2F2F5");
+
+        small.setOnClickListener{
+            resultSize.text = "Маленькая 25 cm,"
+            small.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            ave.backgroundTintList = ColorStateList.valueOf(color_gray)
+            big.backgroundTintList = ColorStateList.valueOf(color_gray)
+
+        }
+        ave.setOnClickListener{
+            resultSize.text = "Средняя 30 cm,"
+            ave.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            small.backgroundTintList = ColorStateList.valueOf(color_gray)
+            big.backgroundTintList = ColorStateList.valueOf(color_gray)
+        }
+        big.setOnClickListener{
+            resultSize.text = "Большая 35 cm,"
+            big.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            ave.backgroundTintList = ColorStateList.valueOf(color_gray)
+            small.backgroundTintList = ColorStateList.valueOf(color_gray)
+        }
+
+        traditional.setOnClickListener {
+            resultDough.text = " традиционное теста"
+            traditional.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            thin.backgroundTintList = ColorStateList.valueOf(color_gray)
+        }
+        thin.setOnClickListener {
+            resultDough.text = " тонкое теста"
+            thin.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+            traditional.backgroundTintList = ColorStateList.valueOf(color_gray)
+        }
+
+        val regex = Regex("[0-9]+")
+        val order: TextView = binding.order
+        val res = resultPrice?.let { regex.find(it) }
+        if (res != null) {
+            order.text = "Заказать пиццу за ${res.value} KZT"
+        }
+
+        val prev_btn : ImageButton = binding.prevBtn
+        prev_btn.setOnClickListener {
+            finish()
+        }
     }
 }
+
+
+
 
